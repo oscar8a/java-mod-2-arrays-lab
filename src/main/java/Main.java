3ea8c27251144e63cb83b93820f81a36ec5075dd
Oscar 8A <oscarochoacerrato@gmail.com>
2022-06-27 14:04:09 -0400

    lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        String[] students = new String[10];
+
+        try (Scanner input = new Scanner(System.in)) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Enter student's name:");
+                String name = input.nextLine();
+                students[i] = name;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (String student : students) {
+            System.out.println("You have entered the following names:");
+            System.out.println("=> " + student);
+        }
     }
 }
